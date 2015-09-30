f08f4009ecd2bfb5940155939b5ff7585fe17598
Jordie Hannel <jordie.hannel@thoughtspot.com>
2015-09-29 22:48:08 -0700

    asdfa

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'rails_12factor', group: :production
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+ruby 2.2.1
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
-Post.delete_all
-Comment.delete_all
-
-User.create!([
-		{id: 1, name: 'Jordie', email: 'lol@gmail.com', password: 'asdfadfsdfsfsfd'}
-	])
-
-Post.create!([
-		{id: 1, body: 'post1', user_id: 1}
-	])
-
-Comment.create!([
-		{id: 1, body: 'comment1', user_id: 1, post_id: 1}
-	])
